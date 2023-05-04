0aea3e811b99084c8600007375fcf72b35d99f5d
GhenaMasri <ghenama77@gmail.com>
2023-05-04 13:41:59 +0300

    Connet MySql

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql');
 const port = 3000;
+
+app.use(express.urlencoded({extended: true})); // New
+// Parse application/json
+app.use(express.json()); // New
+
+const con  = mysql.createConnection({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : '',
+  database        : 'advancedsoftwareproject'
+});
+
+con.connect((err)=>{
+if(err){
+  console.log(err);}
+  else console.log("Connected");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
