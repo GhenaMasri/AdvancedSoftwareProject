57ce715c35322f7f29b3a9eaa315d616306f9ec0
GhenaMasri <ghenama77@gmail.com>
2023-05-05 14:01:50 +0300

    Application Submission

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const mysql = require('mysql');
 const port = 3000;
 
+//for pdf
+const multer = require('multer');
+const upload = multer();
+
 app.use(express.urlencoded({extended: true})); // New
 // Parse application/json
 app.use(express.json()); // New
@@ -23,6 +27,23 @@ if(err){
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+//Application Submittion
+app.post("/ApplicationSubmission/:userId/:jobID",upload.fields([{name: 'resume'}, {name: 'coverletter'}]),(req,res)=>{
+   const userId= req.params.userId; 
+   const jobID = req.params.jobID;
+   const  appID=req.body.id;
+   const pdf1 = req.files['resume'][0].buffer; // First PDF 
+   const pdf2 = req.files['coverletter'][0].buffer; // Second PDF 
+   const query = 'INSERT INTO applications (id, resume, cover_letter, job_id, employee_id) VALUES (?, ?, ?, ?, ?)';
+   con.query(query,[appID,pdf1,pdf2,jobID,userId], (err, results) => {
+    if (err) console.log(err);
+    else res.send('row inserted into database applications table');
+  });
+
+
+
 });
 
 app.listen(port, () => {
