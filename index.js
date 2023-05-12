753e08491f80e5351faaa0a164b77cf02c069c31
MasaSayegh <s11924354@stu.najah.edu>
2023-05-12 16:23:46 +0300

    mm

--- index.js
+++ index.js
@@ -7,12 +7,44 @@ app.use(express.urlencoded({extended: true})); // New
 // Parse application/json
 app.use(express.json()); // New
 
-const con  = mysql.createConnection({
-  connectionLimit : 10,
-  host            : 'localhost',
-  user            : 'root',
-  password        : '',
-  database        : 'advancedsoftwareproject'
+//Routes Initialization
+const appsRoutes = require("./routes/Applications");
+const jobsRoutes = require("./routes/JobSearch");
+
+
+//Using Routes
+app.use("/apps", appsRoutes);
+app.use("/jobs", jobsRoutes);
+
+// Configure passport and LinkedIn strategy
+passport.use(
+  new LinkedInStrategy(
+    {
+      clientID: '7781rvz75h5xk8',
+      clientSecret: 'FRuMDclrr7r9Kh3L',
+      callbackURL: 'http://localhost:3000/auth/linkedin/callback',
+      scope: ['r_liteprofile', 'r_emailaddress' ], // Specify the permissions you require // Specify the permissions you require
+    },
+    (token, refreshToken, profile, done) => {
+      // Store the access token
+
+
+      accessToken = token;
+      return done(null, { accessToken, profile });
+    }
+  )
+);
+
+// Initialize passport middleware
+app.use(passport.initialize());
+
+// Define the authentication route to sign in to linkedIn
+app.get('/auth/linkedin', passport.authenticate('linkedin', { session: false, successRedirect: 'https://www.linkedin.com' }));
+
+
+// Define the callback route to use after sign in successfully
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { session: false }), async (req, res) => {
+  res.redirect('https://www.linkedin.com');
 });
 
 con.connect((err)=>{
