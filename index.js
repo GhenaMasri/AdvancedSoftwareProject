3c155fbf54197cbc676629da1bd4a387834dc1d5
MasaSayegh <s11924354@stu.najah.edu>
2023-05-13 09:40:45 +0300

    CRUD-basic

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const express = require ("express");
+const express = require("express");
+const app = express();
+const mysql = require('mysql');
 const port = 3000;
-const axios = require('axios');
-const passport = require('passport');
-const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
-let accessToken = null;
 
-
-
-const { app, mysql, upload } = require("./config/myConn");
 app.use(express.urlencoded({extended: true})); // New
 // Parse application/json
 app.use(express.json()); // New
@@ -15,13 +10,11 @@ app.use(express.json()); // New
 //Routes Initialization
 const appsRoutes = require("./routes/Applications");
 const jobsRoutes = require("./routes/JobSearch");
-const loginRoutes = require("./routes/login");
 
 
 //Using Routes
 app.use("/apps", appsRoutes);
 app.use("/jobs", jobsRoutes);
-app.use("/logs", loginRoutes);
 
 // Configure passport and LinkedIn strategy
 passport.use(
@@ -54,60 +47,17 @@ app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { session: 
   res.redirect('https://www.linkedin.com');
 });
 
-//External API to search for linkedin jobs
-app.post('/jobsearch', async (req, res) => {
-  const options = {
-    method: 'POST',
-    url: 'https://linkedin-jobs-search.p.rapidapi.com/',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': 'c3f4089899msh47b5fadecce9088p1c9f0bjsn6de18817a9b4',
-      'X-RapidAPI-Host': 'linkedin-jobs-search.p.rapidapi.com'
-    },
-    data: {
-      search_terms: req.body.search_terms,
-      location: req.body.location,
-      page: req.body.page
-    }
-  };
-
-  try {
-    const response = await axios.request(options);
-    return res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'An error occurred during the job search.' });
-  }
+con.connect((err)=>{
+if(err){
+  console.log(err);}
+  else console.log("Connected");
 });
 
-//External API to search for indeed jobs
-app.post('/indeedJobSearch',async(req,res)=>{
-  const axios = require('axios');
-
-const options = {
-  method: 'POST',
-  url: 'https://indeed11.p.rapidapi.com/',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': 'c3f4089899msh47b5fadecce9088p1c9f0bjsn6de18817a9b4',
-    'X-RapidAPI-Host': 'indeed11.p.rapidapi.com'
-  },
-  data: {
-    search_terms: req.body.search_terms,
-    location: req.body.location,
-    page: req.body.page
-  }
-};
-
-try {
-	const response = await axios.request(options);
-	res.json(response.data);
-} catch (error) {
-	console.error(error);
-}
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
